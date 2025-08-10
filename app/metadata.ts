3d67058e590ac1a68d2008331b1062cf8489a96e
HanumantPisal <hanumant.nbnssoe.comp@gmail.com>
2025-08-10 13:37:48 +0530

    first commit

---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ELearning Platform",
+  description: "A modern e-learning platform for students and teachers",
+};
